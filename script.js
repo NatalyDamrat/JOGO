dd6d1f1d83f9ebce8e1c3b0bc9da95d22e57d0c3
Nataly Aparecida Damrat <nataly.damrat@escola.pr.gov.br>
2025-08-08 08:18:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const cardArray = [
+  { name: 'A', img: 'imagens/gato.jpeg' },
+  { name: 'A', img: 'imagens/gato.jpeg' },
+  { name: 'B', img: 'imagens/passaro.jpeg' },
+  { name: 'B', img: 'imagens/passaro.jpeg' },
+  // adicione novos pares aqui
+  { name: 'C', img: 'imagens/cachorro.jpeg' },
+  { name: 'C', img: 'imagens/cachorro.jpeg' }
+  { name: 'D', img: 'imagens/coala.jpeg' },
+  { name: 'D', img: 'imagens/coala.jpeg' }
+  { name: 'E', img: 'imagens/raposas.jpeg' },
+  { name: 'E', img: 'imagens/raposas.jpeg' }
+];
